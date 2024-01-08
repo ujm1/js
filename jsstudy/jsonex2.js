a5504a5ee4e2827397c9a915d783feffb743ad31
jaman <woakswoaks@gmail.com>
2024-01-08 17:53:37 +0900

    0108

--- jsstudy/jsonex2.js
+++ jsstudy/jsonex2.js
@@ -1,38 +1,74 @@
 /* eslint-disable no-undef */
-// fetch('https://jsonplaceholder.typicode.com/todos/1')
+
+// fetch('https://jsonplaceholder.typicode.com/todos')
 // .then(response=>response.json())
 // .then(json=>console.log(json));
-//colpleted가 true인 것들은 왼쪽, false인 것들은 오른쪽
 
 $(function () {
-  $('#btn').on('click', function () {
-    $.get({
-      url: 'https://jsonplaceholder.typicode.com/todos',
-      success: function (result) {
-        result.map(todo => {
-          console.log(todo);
-          if (todo.completed == true) {
-            $('thead').append(
-              $('<tr></tr>')
-                .append($('<td>' + todo.userId + '</td>'))
-                .append($('<td>' + todo.id + '</td>'))
-                .append($('<td>' + todo.title + '</td>'))
-                .append($('<td>' + todo.completed + '</td>'))
-                .append($('<td>' + user.address.street + ' ' + user.address.suite + ' ' + user.address.city + '</td>')),
-            );
-          } //true
-          else {
-            $('thead').append(
-              $('<tr></tr>')
-                .append($('<td>' + todo.userId + '</td>'))
-                .append($('<td>' + todo.id + '</td>'))
-                .append($('<td>' + todo.title + '</td>'))
-                .append($('<td>' + todo.completed + '</td>'))
-                .append($('<td>' + user.address.street + ' ' + user.address.suite + ' ' + user.address.city + '</td>')),
-            );
-          } //false
-        }); //map
-      }, //success
-    }); //get
-  }); //btn
-}); //jquery
+  let a;
+  let cArray = [];
+  let ncArray = [];
+  getData();
+});
+
+const getData = function () {
+  $.get('https://jsonplaceholder.typicode.com/todos')
+    .done(result => procData(result)) //완료되었으면
+    .fail(() => console.log('getData error!!!')); //실패했으면
+};
+
+const procData = function (result) {
+  cArray = result.filter(obj => obj.completed); //조건을 만족하는 값만 뽑아온다
+  ncArray = result.filter(obj => !obj.completed);
+  printData(cArray, ncArray);
+};
+
+const printData = function (cArray, ncArray) {
+  cArray.forEach(obj => {
+    $('#completed ul').append(`<li>${obj.id} ${obj.title}
+      <input type='button' class='cbtn' value='미완료' /></li>`);
+  });
+  change();
+  /////////////////////////////////////////
+  ncArray.forEach(obj => {
+    $('#notcompleted ul').append(`<li>${obj.id} ${obj.title}
+      <input type='button' class='ncbtn' value='완료' /></li>`);
+  });
+  change();
+};
+
+const change = function () {
+  $('.cbtn').on('click', function () {
+    move($(this).parent().attr('id'));
+  });
+
+  $('.ncbtn').on('click', function () {
+    move($(this).parent().attr('id'));
+  });
+};
+
+const move = function (id) {
+  console.log(id);
+};
+
+//   $('#btn').on('click', function () {
+//     $.get({
+//       url: 'https://jsonplaceholder.typicode.com/todos',
+//       success: function (result) {
+//         result.map(todo => {
+//           console.log(todo);
+//           if (todo.completed == 'true') {
+//             $('#completed').append(
+//               $('<ul></ul>').append(todo.userId + ' ' + todo.id + '' + todo.title + ' ' + todo.completed),
+//             );
+//           } //true
+//           else {
+//             $('#notcompleted').append(
+//               $('<ul></ul>').append(todo.userId + ' ' + todo.id + '' + todo.title + ' ' + todo.completed),
+//             );
+//           } //else
+//         }); //map
+//       }, //success
+//     }); //get
+//   }); //btn
+// }); //jquery
